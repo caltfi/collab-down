f15cba6a2090401a0baed1b4f4708eb601fa82dd
Calum Fenton <calum.t.fenton@gmail.com>
2023-08-13 16:47:40 +0100

    git commit -m "Removed commented redund. code"

--- index.php
+++ index.php
@@ -35,41 +35,5 @@
     }
     ?>
 </div>
-<!-- <div class="row">
-    <div class="col">
-        <div class="row">
-            <h1 class="text-center">Markdown Form</h1>
-        </div>
-        <form action="index.php" method="post">
-            <div class="row">
-                <label for="mdContent" aria-label="Markdown Text Goes Here"></label>
-                <textarea name="mdContent" id="mdContent" cols="30" rows="15" placeholder="Markdown Text Here"><?php echo file_get_contents($mdFile);?></textarea>
-            </div>
-            <div class="row d-flex justify-content-center">
-                <div class="col-4">
-                    <input class="btn btn-primary w-100" type="submit" name="submit" value="Save">
-                </div>
-            </div>
-        </form>
-    </div>
-    <div class="col">
-        <div class="row">
-            <h1 class="text-center">Markdown Display</h1>
-        </div>
-        <div class="container">
-            <?php
-                if(isset($_POST['mdContent'])){
-                    $mdText = $_POST['mdContent'];
-                    if(is_readable($mdFile)){
-                        file_put_contents($mdFile, $mdText);
-                        $Parsedown = new Parsedown();
-                        $Parsedown -> setSafeMode(true);
-                        echo $Parsedown->text(file_get_contents($mdFile));
-                    }
-                }
-            ?>
-        </div>
-    </div>
-</div> -->
 <!-- Footer -->
 <?php include "assets/footer.php"; ?>
