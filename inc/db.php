6d38c8266968fa08fd4fdfac869d1b6d048106fe
Calum Fenton <calum.t.fenton@gmail.com>
2023-09-12 15:14:02 +0100

    Updated db.php.

--- inc/db.php
+++ inc/db.php
@@ -1,12 +1,15 @@
 <?php
+    $ini = include 'config.php';
+
     //Connection to mySQL database
-    $servername = 'localhost';
-    $username   = 'root';
-    $password   = '';
-    $dbname     = 'collabdown';
+    $servername = $ini['host'];
+    $username   = $ini['username'];
+    $password   = $ini['password'];
+    $dbname     = $ini['dbname'];
 
     $connection = mysqli_connect($servername, $username, $password, $dbname);
 
     if(!$connection){
-        echo "ERROR: Cannot connect to database. " . mysqli_connect_error();
+        echo "<div class='alert alert-danger alert-dismissible fade show' role='alert'>
+        <strong>Error: Cannot connect to database</strong> " . mysqli_connect_error() . "<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button></div>";
     }
